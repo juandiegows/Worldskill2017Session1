4a3a103bc327dbffbf3f9a1858faadd0f51f6c26
Juan Diego Mejía Maestre <Mejiamaestrejuandiego@gmail.com>
2021-05-09 15:08:37 -0500

    Agregar Clases view para usuarios

---
+++ Worldskill2017Session1/ViewClass/UserView.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Worldskill2017Session1.ViewClass
+{
+    class UserView
+    {
+        public int ID { set; get; }
+        public string Nombre { set; get; }
+        public string Apellido { set; get; }
+        public int Edad { set; get; }
+        public string Rol { set; get; }
+        public string Correo { set; get; }
+        public string Oficina { set; get; }
+        public int Activo { set; get; }
+    }
+}
