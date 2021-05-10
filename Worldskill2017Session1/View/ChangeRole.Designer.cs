5f826236eb118d5ac0b9875d7c0233d4d7e80393
Juan Diego Mejía Maestre <Mejiamaestrejuandiego@gmail.com>
2021-05-10 09:07:52 -0500

    add changerole

---
+++ Worldskill2017Session1/View/ChangeRole.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Worldskill2017Session1.View
+{
+    partial class ChangeRole
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblRole = new System.Windows.Forms.Label();
+            this.cmbRole = new System.Windows.Forms.ComboBox();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnChangeRole = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblRole
+            // 
+            this.lblRole.AutoSize = true;
+            this.lblRole.Font = new System.Drawing.Font("Century Gothic", 24F);
+            this.lblRole.Location = new System.Drawing.Point(82, 20);
+            this.lblRole.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblRole.Name = "lblRole";
+            this.lblRole.Size = new System.Drawing.Size(211, 39);
+            this.lblRole.TabIndex = 22;
+            this.lblRole.Text = "Cambiar Rol";
+            // 
+            // cmbRole
+            // 
+            this.cmbRole.FormattingEnabled = true;
+            this.cmbRole.Location = new System.Drawing.Point(27, 100);
+            this.cmbRole.Name = "cmbRole";
+            this.cmbRole.Size = new System.Drawing.Size(337, 29);
+            this.cmbRole.TabIndex = 21;
+            // 
+            // btnClose
+            // 
+            this.btnClose.BackColor = System.Drawing.Color.Red;
+            this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClose.ForeColor = System.Drawing.Color.White;
+            this.btnClose.Location = new System.Drawing.Point(226, 151);
+            this.btnClose.Margin = new System.Windows.Forms.Padding(5);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(138, 35);
+            this.btnClose.TabIndex = 20;
+            this.btnClose.Text = "Cerrar";
+            this.btnClose.UseVisualStyleBackColor = false;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnChangeRole
+            // 
+            this.btnChangeRole.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnChangeRole.Location = new System.Drawing.Point(27, 151);
+            this.btnChangeRole.Margin = new System.Windows.Forms.Padding(5);
+            this.btnChangeRole.Name = "btnChangeRole";
+            this.btnChangeRole.Size = new System.Drawing.Size(138, 35);
+            this.btnChangeRole.TabIndex = 19;
+            this.btnChangeRole.Text = "Cambiar ";
+            this.btnChangeRole.UseVisualStyleBackColor = true;
+            this.btnChangeRole.Click += new System.EventHandler(this.btnChangeRole_Click);
+            // 
+            // ChangeRole
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(377, 200);
+            this.Controls.Add(this.lblRole);
+            this.Controls.Add(this.cmbRole);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnChangeRole);
+            this.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.MaximizeBox = false;
+            this.Name = "ChangeRole";
+            this.Text = "ChangeRole";
+            this.Load += new System.EventHandler(this.ChangeRole_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblRole;
+        private System.Windows.Forms.ComboBox cmbRole;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnChangeRole;
+    }
+}
---
+++ Worldskill2017Session1/View/ChangeRole.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Worldskill2017Session1.Model;
+
+namespace Worldskill2017Session1.View
+{
+    public partial class ChangeRole : Form
+    {
+
+        private Users Usuario { get; set; }
+        public ChangeRole()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        private void FillRole()
+        {
+            using (Model.Session1Entities model = new Model.Session1Entities())
+            {
+                List<Roles> rol = model.Roles.ToList();
+                cmbRole.DataSource = rol;
+                cmbRole.DisplayMember = "Title";
+                cmbRole.ValueMember = "ID";
+            }
+        }
+
+        private void btnChangeRole_Click(object sender, EventArgs e)
+        {
+            using (Session1Entities model = new Session1Entities())
+            {
+                Users users = model.Users.FirstOrDefault(u => u.ID == Usuario.ID);
+                users.RoleID = (int)cmbRole.SelectedValue;
+                model.Entry(users).State = System.Data.Entity.EntityState.Modified;
+                if (model.SaveChanges() > 0)
+                {
+                    MessageBox.Show("Se ha actualizado el rol");
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("No Se ha actualizado el rol");
+                }
+            }
+        }
+
+        private void ChangeRole_Load(object sender, EventArgs e)
+        {
+            FillRole();
+        }
+    }
+}
